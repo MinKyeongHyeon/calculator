d7718c6e815408ec875601a4d23223590e441fe3
MinKyeonghyeon <28599@naver.com>
2025-07-15 09:47:52 +0900

    fix/ 제곱근과 제곱에 대한 예외처리 추가

--- js/calculator/advance.js
+++ js/calculator/advance.js
@@ -9,5 +9,13 @@ export function power(a, b) {
 // n제곱근 함수
 export function squareRoot(a, b) {
   // TODO4-2: a의 b제곱근을 계산하는 로직을 작성하세요
+  if (a < 0) {
+    throw new Error(
+      '음수의 제곱근은 복소수가 되므로 이 계산기에서는 지원하지 않습니다. 닝겐 🤖'
+    );
+  }
+  if (b === 0) {
+    throw new Error('치지직...⚡️🤖⚡️ 0으로 나눌 수 없습니다. 닝겐.');
+  }
   return a ** (1 / b);
 }
